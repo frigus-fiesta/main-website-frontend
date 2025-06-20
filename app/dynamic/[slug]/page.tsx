0b5aafff3a02d060c0eea5e64fefc4ec596bf49a
Adnan <adnanali23189@gmail.com>
2025-06-20 21:17:30 +0530

    test: dynamic route testings...

--- app/dynamic/[slug]/page.tsx
+++ app/dynamic/[slug]/page.tsx
@@ -1,9 +1,11 @@
+export const runtime = 'edge';
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
   const { slug } = await params
-  
+
   return <div>My Post: {slug}</div>
 }
