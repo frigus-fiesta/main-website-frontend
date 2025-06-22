276cc4de58edc8d2855b376d6f2ee2f15a59135b
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2025-06-22 22:34:56 +0530

    our clients removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/Header";
 import HomeAbout from "@/components/Home/HomeAbout";
 import HomeClients from "@/components/Home/HomeClients";
 import Homepage from "@/components/Home/Homepage";
-import HomeServices from "@/components/Home/HomeServices";
 import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
       <Header/>
       <Homepage/>
       <HomeAbout/>
-      <HomeServices/>
       <HomeClients/>
       <Testimonials/>
     </>
