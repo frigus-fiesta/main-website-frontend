d85ad2d4bc00be3b040104a93466b30b6bf397e8
ArshilAliAbbas <arshil1227@gmail.com>
2025-06-24 09:42:05 -0400

    feat:contact us page structure built

---
+++ app/contactus/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import ContactUsLanding from "@/components/ContactUs/ContactUsLanding"; 
+
+export default function ContactUsPage() {
+  return (
+    <>
+      <Header />
+      <ContactUsLanding />
+    </>
+  );
+}
