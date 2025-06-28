f19c313953061c91072d020ab808ee3620f4ce4b
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2025-06-28 16:09:46 +0530

    eslitn errors fixed in layout.tsx and homepage.tsx

--- components/Home/Homepage.tsx
+++ components/Home/Homepage.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { FaWhatsapp } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 
 const Homepage = () => {
